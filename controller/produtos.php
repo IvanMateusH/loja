f77e2ba340a85f9f825e6cc8ee228d819f12f249
Ivan Mateus Honemann <57683856+IvanMateusH@users.noreply.github.com>
2019-11-12 23:06:13 -0300

    Add files via upload

---
+++ controller/produtos.php
@@ -0,0 +1,32 @@
+
+<?php 
+
+$smarty = new Template();
+
+$produtos = new Produtos();
+
+if(isset(Rotas::$pag[1])){
+	$produtos->GetProdutosCateID(Rotas::$pag[1]);
+
+}else{
+	$produtos->GetProdutos();
+}
+
+
+
+
+
+$smarty->assign('PRO', $produtos->GetItens());
+$smarty->assign('PRO_INFO', Rotas::pag_ProdutosInfo());
+$smarty->assign('PRO_TOTAL', $produtos->TotalDados());
+$smarty->assign('PAGINAS', $produtos->ShowPaginacao());
+
+
+
+
+$smarty->display('produtos.tpl');
+
+
+
+ ?>
+
